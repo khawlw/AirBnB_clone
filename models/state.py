fe6a11db653a6da72060e2fc18afd614d88a95cc
khawlw <142344753+khawlw@users.noreply.github.com>
2024-03-10 12:23:21 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
-
+""" Class State """
 from models.base_model import BaseModel
 
-class State(BaseModel):
-    """Class for managing state objects"""
 
+class State(BaseModel):
+    """ State class that inherits BaseModel"""
     name = ""
